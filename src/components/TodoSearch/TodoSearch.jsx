408563b8ca606365b91d9b3bbd06252c6a6c2b9d
Gualberto <gumodi10@gmail.com>
2022-11-15 20:37:46 -0600

    Add TodoSearch component Event

--- src/components/TodoSearch/TodoSearch.jsx
+++ src/components/TodoSearch/TodoSearch.jsx
@@ -1,19 +1,26 @@
-import React from 'react'
+import { useState } from 'react'
 import './TodoSearch.css'
+
 
 const TodoSearch = () => {
   
-  const onSearchValueChange = (event) => {
-    console.log(event.target.value)
-  }
+  const [searchValue, setSearchValue] = useState('');
 
-  return (
+  const onSearchValueChange = (event) => {
+    console.log(event.target.value);
+    setSearchValue(event.target.value);
+  };
+
+  return [
     <input 
+    key={1}
     className="TodoSearch" 
     placeholder= "¿Buscas un TODO?" 
+    value={searchValue}
     onChange={onSearchValueChange}
-    />
-  )
+    />,
+    <p key={2}>{searchValue}</p>
+  ];
 }
 
 export { TodoSearch }
