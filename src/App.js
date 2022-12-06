46b3165608efc392292afb972ef5bbd100d173af
Gualberto <gumodi10@gmail.com>
2022-12-06 12:40:56 -0600

    Add components and state in React

--- src/App.js
+++ src/App.js
@@ -1,11 +1,61 @@
-import React from 'react';
-import { TodoProvider } from './components/TodoContext/TodoContext';
-import { AppUI }from './AppUI';
+//import React from 'react';
+import {state, useState, useContext} from 'react';
+//import { TodoProvider } from './components/TodoContext/TodoContext';
+//import { AppUI }from './AppUI';
+//import { TodoCounter } from './components/TodoCounter/TodoCounter';
 
-function App(){
+function App() {
+
+  const [state, setState] = useState("estado compartido")
+
+  return (
+    <>
+      <TodoHeader>
+        <TodoCounter />
+        <TodoSearch />
+      </TodoHeader >
+
+      <TodoList>
+        <TodoItem />
+      </TodoList >
+    </>
+
+  )
+}
+
+function TodoHeader({ children }) {
+  return (
+   <header>
+    { children }
+   </header>
+  )
+}
+
+function TodoList({ children }) {
+  return (
+    <section className="TodoList-container">
+      { children }
+    </section>
+
+  )
+}
+
+function TodoCounter() {
+  return <p>TodoCounter</p>
+}
+
+function TodoSearch() {
+  return <p>TodoSearch</p>
+}
+
+function TodoItem({ state }) {
+  return <p>TodoItem: {state}</p>
+}
+
+/* function App(){
   return(
   <TodoProvider>
     <AppUI/>
   </TodoProvider>);}
-
+ */
 export default App;
