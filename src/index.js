2cffd35ababaee448e2f8b38ab11a7f0bfe7d072
Gualberto <gumodi10@gmail.com>
2022-12-08 14:44:17 -0600

    Add HOC practice

--- src/index.js
+++ src/index.js
@@ -1,10 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App.js';
+//import App from './App.js';
 import './index.css';
 
+function App(props) {
+  return (
+    
+      <h1>¡{props.saludo}, {props.nombre}!</h1>
+    
+  );
+}
+
+function withSaludo(WrappedComponent) {
+  return function WrappedComponentWithSaludo(saludo) {
+    return function ComponenteDeVerdad(props) {
+        return (
+            <>
+                <WrappedComponent {...props} saludo={saludo}/>
+                <p>Estamos acompañando al WrappedComponent</p>
+            </>
+        )
+    }
+  }
+}
+
+const AppWithSaludo = withSaludo(App)("Ola Khe Aze");
+
 ReactDOM.render(
-    <App/>,
-    document.getElementById('root'));
+    <AppWithSaludo nombre="gualberto" />,
+    // <App saludo="Que onda" nombre="sr.gualberto"/>,
+    document.getElementById('root')
+    );
 
 
